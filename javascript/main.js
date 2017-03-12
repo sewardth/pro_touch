bd79a79b79c6dad9dbbdfd5e2cb76e8b66b5d442
sewardth <sewardth@gmail.com>
2017-03-12 14:33:14 -0400

    added main page image.  fixed footer to always be at bottom

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,9 @@ $("#quoteSubmission").submit(function( event ) {
   event.preventDefault();
   $("#response").append("submitted").addClass("alert alert-success");
   $("#myModal").fadeOut(5000, function(){
+    $('quoteSubmission').trigger("reset");
     $("#modalClose").click();
+    $("#response").empty().removeClass();
   });
   
 });
