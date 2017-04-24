68cf1ef501418aa3f95082db62b997d23f74647f
sewardth <sewardth@gmail.com>
2017-04-23 20:07:38 -0400

    added sendgrid email platform

--- controllers/quoteSubmissions.py
+++ controllers/quoteSubmissions.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import webapp2
-import google.appengine.api.mail as mail
-from google.appengine.api import app_identity
+from google.appengine.ext import vendor
+vendor.add('lib')
+import sendgrid
+from sendgrid.helpers import mail
+from config import Config
 
 
 class Quote(webapp2.RequestHandler):
@@ -19,10 +22,9 @@ class Quote(webapp2.RequestHandler):
         self.sendMail(**post)
 
 
+
     def sendMail(self, **post):
     
-        sender_address = 'Protouch Quotes <protouchsites@gmail.com>'
-        subject = 'New Quote Request'
         body = """
         name: {}
         email: {}
@@ -31,7 +33,17 @@ class Quote(webapp2.RequestHandler):
         square footage: {}
         rooms: {}""".format(post['name'], post['emailAddress'], post['city'], post['phone'], post['sqFootage'], post['rooms'])
         
-        mail.send_mail(sender_address, 'sewardth@gmail.com', subject, body)
+        sg = sendgrid.SendGridAPIClient(apikey=Config.sendgridApiToken)
+
+        to_email = mail.Email(Config.sendgridRecipient)
+        from_email = mail.Email(Config.sendgridSender)
+        subject = 'New Quote Request'
+        content = mail.Content('text/plain', body)
+        message = mail.Mail(from_email, subject, to_email, content)
+
+        response = sg.client.mail.send.post(request_body=message.get())
+
+        return response
 
 
 
